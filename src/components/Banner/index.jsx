a12827e00f35e739e1e4ffb9dd474fd0b1e87102
Tran Ngoc Minh Thien [timothytnm] <timothytnm@nexondv.com>
2023-05-09 17:51:51 +0700

    confetti

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { ERAS } from "../../data/eras";
 import { useState } from "react";
 import "./../../index.css";
+import { Star1, Star2, Star3, Star4, Star5, Star6 } from "../Stars";
+import ErasParty from "../ErasParty";
+import Confetti from "react-confetti";
+const ALBUM_TIME = 10000;
+const TRACK_TIME = 10000;
 const Container = styled.div`
   width: 100%;
-  max-height: 100vh;
   display: flex;
   flex-direction: column;
   position: relative;
 `;
 const Banner = styled.div`
   width: 100%;
+  max-height: 98vh;
   display: flex;
   flex-direction: row;
   position: relative;
@@ -60,7 +65,7 @@ const EraButton = styled.div`
       : props.active === true
       ? `50%`
       : `${50 / 9}%`};
-  height: calc(100vh);
+  height: 100vh;
   color: ${(props) => props.txt};
   cursor: pointer;
 `;
@@ -68,7 +73,7 @@ const AlbumName = styled.div`
   color: ${(props) => (props.isRandomAlbum === false ? "white" : "")};
   z-index: 1;
   user-select: none;
-  font-size: 45px !important;
+  font-size: ${(props) => (props.active ? "70px" : "45px")} !important;
   writing-mode: ${(props) =>
     props.active === false || props.isRandomAlbum === false
       ? "vertical-lr"
@@ -79,6 +84,10 @@ const AlbumName = styled.div`
   height: ${(props) => (props.active === false ? "100%" : "")};
   transform: ${(props) =>
     props.active === false ? "translate(-50%, -50%)" : "translate(0, 0)"};
+  filter: ${(props) =>
+    props.active === true && props.isRandomAlbum === false
+      ? "drop-shadow(0 0 13px #fffdef)"
+      : ""};
 `;
 const Tracks = styled.div`
   z-index: 1;
@@ -90,9 +99,49 @@ const Tracks = styled.div`
   font-family: Verdana, Geneva, Tahoma, sans-serif;
 `;
 const Track = styled.div`
+  position: relative;
   user-select: none;
-  font-size: ${(props) => (props.trackActive === true ? "20px" : "14px")};
+  font-size: ${(props) => (props.trackActive === true ? "30px" : "18px")};
   font-weight: ${(props) => (props.trackActive === true ? "700" : "300")};
+  z-index: ${(props) => (props.trackActive === true ? "99" : "0")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  filter: drop-shadow(
+    0 0 5px
+      ${(props) => (props.trackActive === true ? props.color : "transparent")}
+  );
+  .star-1,
+  .star-2,
+  .star-3 {
+    filter: drop-shadow(0 0 5px ${(props) => props.color});
+    z-index: 3;
+    height: auto;
+    width: 10px;
+    display: ${(props) => (props.trackActive === true ? "block" : "none")};
+  }
+  .star-1 {
+    left: 100%;
+    width: 26px;
+    transition: all 1s cubic-bezier(0.05, 0.83, 0.43, 0.96);
+  }
+
+  .star-2 {
+    left: 0%;
+    width: 13px;
+    transform: translateY(20%);
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+  .star-3 {
+    left: 0%;
+    width: 19px;
+    transform: translateY(-20%);
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+  .fil0 {
+    fill: ${(props) => props.color};
+  }
 `;
 const TaylorSwift = styled.img`
   display: ${(props) => (props.active === false ? "none" : "block")};
@@ -106,21 +155,114 @@ const TaylorSwift = styled.img`
   opacity: 0.5;
 `;
 const Random = styled.div`
+  --glow-color: rgb(217, 176, 255);
+  --glow-spread-color: rgba(191, 123, 255, 0.781);
+  --enhanced-glow-color: rgb(231, 206, 255);
+  --btn-color: rgb(100, 61, 136);
   display: ${(props) => (props.randomButton === false ? "none" : "block")};
   user-select: none;
   cursor: pointer;
   position: absolute;
   z-index: 3;
-  width: 100px;
-  height: 100px;
-  left: 50%;
-  bottom: 5%;
+  width: 60px;
+  height: auto;
   transform: translate(-50%, -50%);
   background-color: transparent;
   border-radius: 50%;
   font-family: "Midnights";
   line-height: 100px;
   font-size: 50px;
+  .star-1,
+  .star-2,
+  .star-3,
+  .star-4,
+  .star-5,
+  .star-6 {
+    position: absolute;
+    z-index: -5;
+    height: auto;
+    top: 50%;
+    left: 50%;
+    width: 10px;
+    transform: translate(-50%, -50%);
+  }
+  .star-1 {
+    transition: all 1s cubic-bezier(0.05, 0.83, 0.43, 0.96);
+  }
+
+  .star-2 {
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+
+  .star-3 {
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+
+  .star-4 {
+    transition: all 0.8s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+
+  .star-5 {
+    transition: all 0.6s cubic-bezier(0, 0.4, 0, 1.01);
+  }
+
+  .star-6 {
+    transition: all 0.8s ease;
+  }
+
+  :hover {
+    background: transparent;
+    filter: drop-shadow(0 0 7px #fffdef);
+    .star-1,
+    .star-2,
+    .star-3,
+    .star-4,
+    .star-5,
+    .star-6 {
+      filter: drop-shadow(0 0 7px #fffdef);
+      position: absolute;
+      z-index: 4;
+      height: auto;
+    }
+    .star-1 {
+      top: -80%;
+      left: -30%;
+      width: 25px;
+    }
+
+    .star-2 {
+      top: -25%;
+      left: 10%;
+      width: 15px;
+    }
+
+    .star-3 {
+      top: 55%;
+      left: -30%;
+      width: 50px;
+    }
+
+    .star-4 {
+      top: 30%;
+      left: 100%;
+      width: 10px;
+    }
+
+    .star-5 {
+      top: -40%;
+      left: 115%;
+      width: 30px;
+    }
+
+    .star-6 {
+      top: 60%;
+      left: 140%;
+      width: 20px;
+    }
+  }
+  .fil0 {
+    fill: #fffdef;
+  }
 `;
 const ErasBanner = () => {
   const [active, setActive] = useState(null);
@@ -131,6 +273,46 @@ const ErasBanner = () => {
   const [albumTracks, setAlbumTracks] = useState([]);
   const [trackActive, setTrackActive] = useState(null);
   const [randomButton, setRandomButton] = useState(true);
+  const [position, setPosition] = useState({
+    x: window.innerWidth * 0.5,
+    y: window.innerHeight * 0.93,
+  });
+  const [dragging, setDragging] = useState(false);
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const confetiRef = useRef(null);
+
+  useEffect(() => {
+    if (trackActive !== null) {
+      setHeight(confetiRef.current.clientHeight);
+      setWidth(confetiRef.current.clientWidth);
+    }
+  }, [trackActive]);
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (dragging) {
+        setPosition({
+          x: position.x + event.movementX,
+          y: position.y,
+        });
+      }
+    };
+    const handleMouseUp = () => {
+      setDragging(false);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [dragging, position]);
+
+  const handleMouseDown = () => {
+    setDragging(true);
+  };
   const handleClick = () => {
     if (!isRunning) {
       setIsRunning(true);
@@ -145,34 +327,35 @@ const ErasBanner = () => {
   useEffect(() => {
     if (isRandomAlbum) {
       setAlbumTracks(ERAS[active].tracks);
-      setTrackActive(ERAS[active].tracks.items[0]);
       setIsTrackRunning(true);
       setIntervalDuration(200);
     }
   }, [isRandomAlbum]);
   useEffect(() => {
     if (isTrackRunning === true) {
-      const interval = setInterval(() => {
-        const randomIndex = Math.floor(
-          Math.random() * albumTracks.items.length
-        );
-        setTrackActive(albumTracks.items[randomIndex]);
-      }, intervalDuration);
       setTimeout(() => {
-        clearInterval(interval);
-        // Select a final element after 10 seconds
-        const finalIndex = Math.floor(Math.random() * 10);
-        setTrackActive(albumTracks.items[finalIndex]);
-        setIsTrackRunning(false);
-        setRandomButton(true);
-      }, 10000);
-      const intervalDurationTimeout = setInterval(() => {
-        setIntervalDuration((prevDuration) => prevDuration + 100);
-      }, 200);
-      return () => {
-        clearInterval(interval);
-        clearInterval(intervalDurationTimeout);
-      };
+        const interval = setInterval(() => {
+          const randomIndex = Math.floor(
+            Math.random() * albumTracks.items.length
+          );
+          setTrackActive(albumTracks.items[randomIndex]);
+        }, intervalDuration);
+        setTimeout(() => {
+          clearInterval(interval);
+          // Select a final element after 10 seconds
+          const finalIndex = Math.floor(Math.random() * 10);
+          setTrackActive(albumTracks.items[finalIndex]);
+          setIsTrackRunning(false);
+          setRandomButton(true);
+        }, TRACK_TIME);
+        const intervalDurationTimeout = setInterval(() => {
+          setIntervalDuration((prevDuration) => prevDuration + 100);
+        }, 200);
+        return () => {
+          clearInterval(interval);
+          clearInterval(intervalDurationTimeout);
+        };
+      }, 2000);
     }
   }, [isTrackRunning]);
   useEffect(() => {
@@ -184,15 +367,14 @@ const ErasBanner = () => {
 
       setTimeout(() => {
         clearInterval(interval);
-        // Select a final element after 10 seconds
         const finalIndex = Math.floor(Math.random() * 10);
         setActive(finalIndex);
         setIsRunning(false);
         setIsRandomAlbum(true);
-      }, 10000);
+      }, ALBUM_TIME);
       const intervalDurationTimeout = setInterval(() => {
         setIntervalDuration((prevDuration) => prevDuration + 100);
-      }, 200);
+      }, 300);
       return () => {
         clearInterval(interval);
         clearInterval(intervalDurationTimeout);
@@ -201,7 +383,10 @@ const ErasBanner = () => {
   }, [isRunning]);
   return (
     <Container>
-      <Banner>
+      <Banner ref={confetiRef}>
+        {trackActive !== null && isTrackRunning === false && isRandomAlbum && (
+          <Confetti numberOfPieces={150} width={width} height={height} />
+        )}
         {ERAS.map((eras, i) => {
           return (
             <EraButton
@@ -220,7 +405,9 @@ const ErasBanner = () => {
             >
               <AlbumName active={active === i} isRandomAlbum={isRandomAlbum}>
                 <a
-                  href={trackActive !== null ? eras.sharingInfo.shareUrl : ""}
+                  href={
+                    trackActive !== null ? eras.sharingInfo.shareUrl : "none"
+                  }
                   target="_blank"
                   style={{
                     textDecoration: "none",
@@ -236,8 +423,16 @@ const ErasBanner = () => {
                   trackActive !== null &&
                   eras.tracks.items.map((track, i) => {
                     return (
-                      <Track key={i} trackActive={track === trackActive}>
+                      <Track
+                        key={i}
+                        trackActive={track === trackActive}
+                        color={eras.color2}
+                      >
+                        <Star1 />
                         {track.track.name}
+
+                        <Star2 />
+                        <Star3 />
                       </Track>
                     );
                   })}
@@ -246,14 +441,19 @@ const ErasBanner = () => {
             </EraButton>
           );
         })}
-        <Random onClick={handleClick} randomButton={randomButton}>
-          <img
-            src="./images/erasParty.png"
-            alt="erasParty"
-            style={{
-              height: "80px",
-            }}
-          />
+        <Random
+          style={{ position: "absolute", left: position.x, top: position.y }}
+          onDoubleClick={handleClick}
+          randomButton={randomButton}
+          onMouseDown={handleMouseDown}
+        >
+          <ErasParty />
+          <Star1 />
+          <Star2 />
+          <Star3 />
+          <Star4 />
+          <Star5 />
+          <Star6 />
         </Random>
       </Banner>
     </Container>
---
+++ src/components/ErasParty/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function ErasParty() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 470.47 615.67">
+      <defs>
+        <style>
+          {`
+          .cls-1{fill:#c9d7c0;}
+          .cls-2{fill:#f4d3a4;}
+          .cls-3{fill:#7b5c80;}
+          .cls-4{fill:#67303b;}
+          .cls-5{fill:#b7daf3;}
+          .cls-6{fill:#635e61;}
+          .cls-7{fill:#f1c3d6;}
+          .cls-8{fill:#d4d2cd;}
+          .cls-9{fill:#c7b199;}
+          .cls-10{fill:#414660;}
+          `}
+        </style>
+      </defs>
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="Layer_1-2" data-name="Layer 1">
+          <circle className="cls-1" cx="69.78" cy="69.78" r="69.78" />
+          <circle className="cls-2" cx="235.24" cy="69.78" r="69.78" />
+          <circle className="cls-3" cx="400.69" cy="69.78" r="69.78" />
+          <circle className="cls-4" cx="69.78" cy="228.48" r="69.78" />
+          <circle className="cls-5" cx="400.69" cy="228.48" r="69.78" />
+          <circle className="cls-6" cx="69.78" cy="387.18" r="69.78" />
+          <circle className="cls-7" cx="400.69" cy="387.18" r="69.78" />
+          <circle className="cls-8" cx="69.78" cy="545.88" r="69.78" />
+          <circle className="cls-9" cx="400.69" cy="545.88" r="69.78" />
+          <path
+            className="cls-10"
+            d="M305,228.48V545.89a69.79,69.79,0,0,1-139.58,0V228.48a69.79,69.79,0,0,1,139.58,0Z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default ErasParty;
